480c40f8e45c0f5f067ac9e50f7d1cefca61677b
devMarcosVM <marcosvieira.2003@gmail.com>
2025-10-26 21:27:43 -0300

    improve: refatora repositório de usuários para maior legibilidade e modularidade

--- src/users/repository.py
+++ src/users/repository.py
@@ -4,18 +4,19 @@ from ..utils import hash_password
 
 # Lógica de acesso a dados
 
+
 class UserRepository:
     def get_user_by_email(self, db: Session, email: str):
         return db.query(model.User).filter(model.User.email == email).first()
+
     def get_user_by_username(self, db: Session, username: str):
         return db.query(model.User).filter(model.User.username == username).first()
+
     def create_user(self, db: Session, user: schema.UserCreate):
         hashed_pass = hash_password(user.password)
-        
+
         db_user = model.User(
-            username=user.username,
-            email=user.email,
-            password_hash=hashed_pass
+            username=user.username, email=user.email, password_hash=hashed_pass
         )
         db.add(db_user)
         db.commit()
