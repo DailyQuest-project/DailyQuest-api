14283771eb2925e82a5e50a36ffc07935ea53422
devMarcosVM <marcosvieira.2003@gmail.com>
2025-10-26 21:27:50 -0300

    fix: corrige rotas de usuário e adiciona validações extras

--- src/users/router.py
+++ src/users/router.py
@@ -7,28 +7,25 @@ from .repository import UserRepository
 from ..users.model import User as UserModel
 from ..deps import get_db, get_current_user
 
-router = APIRouter(
-    prefix="/users",
-    tags=["Users"]
-)
+router = APIRouter(prefix="/users", tags=["Users"])
 
-@router.post("/", response_model=schema.User)
+
+@router.post("/", response_model=schema.User, status_code=status.HTTP_201_CREATED)
 def create_user(
-    user: schema.UserCreate, 
+    user: schema.UserCreate,
     db: Session = Depends(get_db),
-    repo: UserRepository = Depends()
-):
+    repo: UserRepository = Depends(),
+) -> schema.User:
     db_user = repo.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, 
-            detail="Email already registered"
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered"
         )
     return repo.create_user(db=db, user=user)
 
+
 @router.get("/me", response_model=schema.User)
 async def read_users_me(
-    current_user: UserModel = Depends(get_current_user)
-):
+    current_user: UserModel = Depends(get_current_user),
+) -> UserModel:
     return current_user
-
